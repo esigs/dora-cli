83b408d6b6f32729d4c2f8b6510fe90dc72373ef
Eric Sigurdson <eric@ericsigurdson.com>
2025-08-31 22:32:32 -0400

    init

---
+++ nix/devshell-dev.nix
@@ -0,0 +1,20 @@
+{ pkgs }:
+
+let 
+in pkgs.mkShell {
+        buildInputs = [ 
+		pkgs.dotnetCorePackages.sdk_9_0
+		pkgs.nodejs
+        ];
+
+
+        shellHook = ''
+    export APP_ENV="DEV"
+    export PATH=$PWD/.nix-profile/bin:$PATH
+    export PS1="[\033[1;32mNIX-ENV\033[0m $APP_ENV] $PS1"
+
+echo "
+environment:    $APP_ENV
+"
+    '';
+    }
